cca054722fc360ba2f77307e08192b41890a7ce2
Erez Reuven <erez.reuven@gmail.com>
2017-08-13 11:57:21 +0300

    initial

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import {translate} from 'react-i18next';
+// import PropTypes from 'prop-types';
+import s from './App.scss';
+
+function App() {
+  return (
+    <div className={s.root}>
+      <h2>Hiiiiii</h2>
+    </div>
+  );
+}
+
+export default App;
